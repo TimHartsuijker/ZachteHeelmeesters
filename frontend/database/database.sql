8ee9ae5aec688c27eac17ad7f6a13e93088c0fd2
Tim Hartsuijker <tim1hartsuijker@gmail.com>
2025-12-01 15:45:32 +0100

    dist folder removed from gitignore

--- frontend/database/database.sql
+++ frontend/database/database.sql
@@ -1,10 +1,52 @@
-USE master;
-GO
+DROP DATABASE IF EXISTS zachteheelmeester;
 
-DROP DATABASE IF EXISTS ZachteHeelMeester;
+CREATE database zachteheelmeester;
 
-CREATE DATABASE ZachteHeelMeester;
+USE zachteheelmeester;
 
--- uncommenten wanneer iedereen de eerste creatie heeft uitgevoerd.
--- USE ZachteHeelMeester;
--- GO
+drop table IF EXISTS behandelingen;
+
+create table behandelingen (
+	zorgcode varchar(50) NOT NULL,
+    omschrijving varchar(255) NOT NULL,
+    specialisme varchar(20) NOT NULL,
+    tijdsduur int(2) NOT NULL,
+    kosten FLOAT NOT NULL,
+    PRIMARY KEY (zorgcode)
+);
+
+DROP TABLE IF EXISTS rollen;
+
+CREATE TABLE rollen(
+    rolID VARCHAR(50) NOT NULL,
+    rolnaam VARCHAR(100) NOT NULL,
+    PRIMARY KEY (rolID)
+);
+
+DROP TABLE IF EXISTS gebruikers;
+
+CREATE TABLE gebruikers(
+    email VARCHAR(100) NOT NULL,
+    wachtwoord VARCHAR(100) NOT NULL,
+    rol VARCHAR(50) NOT NULL,
+    FOREIGN KEY (rol) REFERENCES rollen(rolID),
+    PRIMARY KEY (email)
+);
+
+DROP TABLE IF EXISTS afdelingen;
+
+CREATE TABLE afdelingen(
+afdelingID VARCHAR(50) NOT NULL,
+naam VARCHAR(100) NOT NULL,
+PRIMARY KEY (afdelingID)
+);
+
+DROP TABLE IF EXISTS afspraken;
+
+CREATE TABLE afspraken(
+datumtijd DATETIME not null,
+behandeling VARCHAR(50) NOT NULL,
+afdeling VARCHAR(50) NOT NULL,
+FOREIGN KEY (behandeling) REFERENCES behandelingen(zorgcode),
+FOREIGN KEY (afdeling) REFERENCES afdelingen(afdelingID)
+);

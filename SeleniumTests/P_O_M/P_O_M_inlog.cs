874d820aa334773ee1ffbcd3b413f7da6fca18d7
kenanr13 <k.redzovic13@hotmail.com>
2025-12-02 13:57:19 +0100

    pom/test project

---
+++ SeleniumTests/P_O_M/P_O_M_inlog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests.P_O_M
+{
+    internal class P_O_M_inlog
+    {
+    }
+}
---
+++ SeleniumTests/Tests/US1/1.19/1_19_1.cs
@@ -0,0 +1,10 @@
+namespace SeleniumTests;
+
+[TestClass]
+public class _1._19._1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}

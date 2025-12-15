7153d31b4c8d05ad373fdb1ae4b0d246a1b45a13
Mar-Jin <marijnhoef@gmail.com>
2025-12-15 23:29:13 +0100

    Model Classes

---
+++ backend/Models/MedicalRecordEntry.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    [PrimaryKey(nameof(Id), nameof(PatientId))]
+    public class MedicalRecordEntry
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int PatientId { get; set; }
+        public User Patient { get; set; } = null!;
+
+        public int TreatmentId { get; set; }
+        public Treatment Treatment { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ backend/Models/Specialism.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Specialism
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Treatment> Treatments { get; set; } = []; // NP for treatments under this specialism
+    }
+}

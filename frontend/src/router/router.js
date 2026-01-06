e366471c21a4e1468d0ff05885a45a391ef2ee41
Jule Jacob <Jule.Jacob@ewe.net>
2026-01-06 19:31:00 +0100

    feat: Add AgendaView component with navigation and DoctorCalendar integration

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import AgendaView from '../views/AgendaView.vue'
 
 const routes = [
   { 
     path: '/', 
     name: 'login',
     component: LoginView
+  },
+  {
+    path: '/agenda',
+    name: 'agenda',
+    component: AgendaView
   }
 ]
 

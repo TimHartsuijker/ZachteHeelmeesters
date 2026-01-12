ee2a9e523187d181095183adcddf2d8b82dc5f30
kenanr13 <k.redzovic13@hotmail.com>
2026-01-12 15:05:36 +0100

    verbetert

--- backend/Controllers/DoctorsController.cs
+++ backend/Controllers/DoctorsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using backend.DTOs;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,13 +20,18 @@ namespace backend.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetDoctors()
         {
             try
             {
                 var doctors = await _context.Users
-                   .OrderBy(r => r.Id)
-                   .Where(u => u.Role.RoleName == "Huisarts")
+                    .Where(u => u.RoleId == 3) 
+                    .Select(u => new DoctorResponseDto
+                    {
+                        Id = u.Id,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName
+                    })
                    .ToListAsync();
                 return Ok(doctors);
             }

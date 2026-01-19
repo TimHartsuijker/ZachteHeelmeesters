ab1a26ea5876eb6cacb23411093445e84275d24f
Mar-Jin <marijnhoef@gmail.com>
2026-01-19 13:46:02 +0100

    Added admin user

--- backend/Data/DbSeederStatic.cs
+++ backend/Data/DbSeederStatic.cs
@@ -1,4 +1,5 @@
 ﻿using backend.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace backend.Data
 {
@@ -7,6 +8,8 @@ namespace backend.Data
         public static void Seed(AppDbContext context)
         {
             context.Database.EnsureCreated();
+
+            var passwordHasher = new PasswordHasher<User>();
 
             // Rollen
             if (!context.Roles.Any())
@@ -18,7 +21,37 @@ namespace backend.Data
                     new Role {RoleName = "Specialist" }
                 );
             }
-           
+
+            var adminRole = context.Roles.FirstOrDefault(r => r.RoleName == "Admin");
+            var adminUser = context.Users.FirstOrDefault(u => u.Email == "admin@zhm.com");
+            if (adminUser == null)
+            {
+                adminUser = new User
+                {
+                    FirstName = "Jaap",
+                    LastName = "Ros",
+                    Email = "admin@zhm.com",
+                    StreetName = "Onderwijsboulevard",
+                    HouseNumber = "215",
+                    PostalCode = "5223DE",
+                    PhoneNumber = "0612345678",
+                    DateOfBirth = DateTime.Parse("2005-06-26"),
+                    Gender = "Man",
+                    CitizenServiceNumber = "123456789",
+                    CreatedAt = DateTime.UtcNow,
+                    RoleId = adminRole.Id
+                };
+
+                adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "AdminWachtwoord123!");
+                context.Users.Add(adminUser);
+                context.SaveChanges();
+                Console.WriteLine($"[DbSeederTest] Created Patient user with ID: {adminUser.Id}");
+            }
+            else
+            {
+                Console.WriteLine($"[DbSeederTest] Patient user already exists with ID: {adminUser.Id}");
+            }
+
             //// Departments
             //if (!context.Departments.Any())
             //{

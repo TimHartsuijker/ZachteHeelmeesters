a88a237429c4dccf50eb8690764c6630a4ec2e3e
Mar-Jin <marijnhoef@gmail.com>
2026-01-25 21:39:10 +0100

    Unnecessary Usings

--- backend/Controllers/LoginController.cs
+++ backend/Controllers/LoginController.cs
@@ -3,11 +3,6 @@ using backend.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace backend.Controllers
 {
